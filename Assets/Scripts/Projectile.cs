cb218af9353342057c5a8a31d81f7e6ebd3e69a4
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2019-09-05 06:51:24 -0700

    feat: Update ui

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -8,9 +8,6 @@ public class Projectile : MonoBehaviour
     void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "Target") Destroy(collision.gameObject);
-        if (collision.gameObject.tag == "Boundary")
-        {
-            Destroy(gameObject);
-        }
+        if (collision.gameObject.tag == "Boundary") Destroy(gameObject);
     }
 }
