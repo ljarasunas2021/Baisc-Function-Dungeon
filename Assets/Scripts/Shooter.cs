00a8f2fb82f341b989b2b83a0fc03c6a82821c90
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2019-09-05 18:27:21 -0700

    feat: Add visualizer

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using System.Collections.Generic;
 using B83.ExpressionParser;
 using TMPro;
 
@@ -7,18 +8,71 @@ public class Shooter : MonoBehaviour
 {
     public TMP_InputField inputField;
     public GameObject projectile;
-    public int maxFrames;
+    public GameObject visualizeProjectile;
     public float speed;
+    public float visualizerSpeed;
+    public int visualizerMaxFrames;
     public GameObject uIController;
     private UIController uIControllerScript;
+    private bool visualizeOn = true;
+    private string lastEquation;
+    private List<GameObject> visualizeProjectileInstants = new List<GameObject>();
 
     private void Start()
     {
         uIControllerScript = uIController.GetComponent<UIController>();
     }
 
+    private void Update()
+    {
+        if (visualizeOn)
+        {
+            try
+            {
+                if (inputField.text != lastEquation)
+                {
+                    var parser = new ExpressionParser();
+                    Expression equation = parser.EvaluateExpression(inputField.text);
+                    Visualize(equation);
+                }
+            }
+            catch
+            {
+                DeleteVisualizeInstants();
+            }
+        }
+    }
+
+    private void Visualize(Expression equation)
+    {
+        DeleteVisualizeInstants();
+
+        bool positiveWorking = true;
+        for (int frames = 0; frames < visualizerMaxFrames && positiveWorking; frames++)
+        {
+            equation.Parameters["x"].Value = frames * visualizerSpeed;
+            float output = (float)equation.Value;
+            if (float.IsNaN(output) || float.IsInfinity(output)) positiveWorking = false;
+            else visualizeProjectileInstants.Add(Instantiate(visualizeProjectile, new Vector3(transform.position.x + frames * visualizerSpeed, transform.position.y, transform.position.z + output), Quaternion.identity));
+        }
+
+        bool negativeWorking = true;
+        for (int frames = 1; frames < visualizerMaxFrames && negativeWorking; frames++)
+        {
+            equation.Parameters["x"].Value = -frames * visualizerSpeed;
+            float output = (float)equation.Value;
+            if (float.IsNaN(output) || float.IsInfinity(output)) negativeWorking = false;
+            else visualizeProjectileInstants.Add(Instantiate(visualizeProjectile, new Vector3(transform.position.x - frames * visualizerSpeed, transform.position.y, transform.position.z + output), Quaternion.identity));
+        }
+
+        lastEquation = inputField.text;
+    }
+
     public void Shoot()
     {
+        visualizeOn = false;
+        DeleteVisualizeInstants();
+
         var parser = new ExpressionParser();
         try
         {
@@ -81,5 +135,10 @@ public class Shooter : MonoBehaviour
         uIControllerScript.InstantiateErrorInInput();
         if (projectileInstantPositive != null) Destroy(projectileInstantPositive);
         if (projectileInstantNegative != null) Destroy(projectileInstantNegative);
+    }
+
+    private void DeleteVisualizeInstants()
+    {
+        foreach (GameObject projectileInstant in visualizeProjectileInstants) Destroy(projectileInstant);
     }
 }
